96e86b6afa71949a2b94173cf77dff761bd8bb3a
123Stan-The-Man123 <Molsas2020@gmail.com>
2024-06-04 22:20:21 +0100

    Trying out variables and printing them!

---
+++ variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Create variables
+    int myNum = 15;         // Integer
+    float myFloat = 6.9;    // Floating point number
+    char myChar = 'S';      //Character
+
+    // Print variables
+    printf("%d\n", myNum);
+    printf("%f\n", myFloat);
+    printf("%c\n", myChar);
+
+    //End main()
+    return 0;
+}
