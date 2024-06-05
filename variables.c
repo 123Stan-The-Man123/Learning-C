95d646c08d3f531a17ba672b4bf36df975f7c3a7
123Stan-The-Man123 <Molsas2020@gmail.com>
2024-06-05 11:58:17 +0100

    Learnt you can assign a variable to another variable! :O

--- variables.c
+++ variables.c
@@ -13,8 +13,14 @@ int main() {
 
     //Print text and variables
     printf("The number stored in \"myNum\" is: %d\n", myNum);           //One data type
-    printf("My float is: %f\nMy character is: %c", myFloat, myChar);    //Multiple data types
+    printf("My float is: %f\nMy character is: %c\n", myFloat, myChar);    //Multiple data types
 
+    // You can assign a variable to a variable :O
+    int favouriteNum = 5;
+    myNum = favouriteNum;
+
+    printf("The value in \"myNum\" has gone from \"%d\" to \"%d\"!", 15, myNum);
+    
     //End main()
     return 0;
 }
