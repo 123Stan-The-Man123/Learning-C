4e6ac704b082275dc9a02998aca43e2b25204b5a
123Stan-The-Man123 <Molsas2020@gmail.com>
2024-06-12 14:57:35 +0100

    Booleans

---
+++ Booleans/boolean.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool boolean = true;
+    bool boolean_1 = false;
+
+    printf("%d\n", boolean);
+    printf("%d\n", boolean_1);
+    printf("%d", 5 == 5);
+
+}
+
+
